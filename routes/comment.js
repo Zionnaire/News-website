aa3264422d446a35626a2fdacf0e47ae6b75a944
Zionnaıre <zionnaire2018@gmal.com>
2023-09-21 19:51:18 +0100

    Comment

--- routes/comment.js
+++ routes/comment.js
@@ -2,26 +2,56 @@ const express = require('express');
 const Comment = require('../models/comment');
 const commentRouter = express.Router();
 
-// Create a new comment
-commentRouter.post('/', async (req, res) => {
+// Create a new comment for a specific content
+commentRouter.post('/:contentId/comments', async (req, res) => {
   try {
+    const { contentId } = req.params; // Get the content ID from the URL
     const { content, video, image, author, user } = req.body;
-    const newComment = await Comment.create({ content, video, image, author, user });
+
+    // Check if the content with the given ID exists
+    const existingContent = await Content.findById(contentId);
+
+    if (!existingContent) {
+      return res.status(404).json({ message: 'Content not found' });
+    }
+
+    // Create a new comment associated with the content
+    const newComment = await Comment.create({
+      content: contentId, // Associate the comment with the content
+      contentType: existingContent.category, // You can store the content type if needed
+      content, video, image, author, user
+    });
+
     res.status(201).json(newComment);
   } catch (error) {
+    console.error('Comment Error:', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
-// Get all comments
+
+// Get all comments for a specific content or implement pagination
 commentRouter.get('/', async (req, res) => {
   try {
-    const comments = await Comment.find();
+    const { contentId } = req.query; // Get the content ID from query parameters if provided
+
+    let comments;
+
+    if (contentId) {
+      // If contentId is provided, fetch comments associated with that content
+      comments = await Comment.find({ content: contentId });
+    } else {
+      // If no contentId is provided, fetch all comments (consider pagination for scalability)
+      comments = await Comment.find();
+    }
+
     res.json(comments);
   } catch (error) {
+    console.error('Comment Error:', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
+
 
 // Create a new reply to a comment
 commentRouter.post('/:commentId/replies', async (req, res) => {
